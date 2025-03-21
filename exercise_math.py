3bdeca7629bfbabaef7039b383c65c667a3bafe4
claradevalais <cdevalais@mail.austral.edu.ar>
2025-03-21 15:46:43 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -3,10 +3,8 @@ def math():
     b = 7
     print = (a+b)
     print = (a-b)
-    print = (a*b)
+    print = (a * b)
     print = ((a+b)/2)
     print = (a//b)
     print = (a%b)
-    print = (a/b)
-    
-    
+    print = (a/b)      
